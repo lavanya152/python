bb780b865f949f8839a939993327aa4c28d92d32
lavanya152 <51982986+lavanya152@users.noreply.github.com>
2022-06-09 09:20:10 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,11 @@
+st1 = input("Enter string1:")
+st2 = input("Enter string2:")
+if len(st1)==len(st2):
+    s1 = sorted(list(st1))
+    s2 = sorted(list(st2))
+    if s1 == s2:
+        print(st1 +" and "+st2 +" are anagrams")
+    else:
+        pass
+else:
+    print(st1 +" and "+st2+ " are not anagrams")
